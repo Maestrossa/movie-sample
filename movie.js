f7f84cc88f22491bac8f1b15c4b0f22e17df3ddb
Maestrossa <s-i-s-e-u-n-g@hanmail.net>
2024-04-26 21:19:03 +0900

    update

--- movie.js
+++ movie.js
@@ -8,86 +8,78 @@ const options = {
   },
 };
 
-let topMovies;
-let movieMap = new Map();
+let movies = [];
 
 fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
   .then((response) => response.json())
   .then((response) => {
-    let movies = response['results']; //assign the json data to "movies"
-    topMovies = response['results']; // assign it to "map"
-    console.log(response['results']);
+    //assign the result data to 'movies'
+    movies = response.results;
 
-    //extract the arrays from 'results' and assign each of them to a card
-    movies.forEach((a) => {
-      createMovieCard(a);
+    //with the results assigned to 'movies', makde a function that
+    //each movie's data to be placed
+    movies.forEach((data) => {
+      createMovieCard(data);
     });
   });
 
-//fetch ID of HTML
-let movieCardPost = document.querySelector('.movieCards');
-console.log(movieCardPost);
+let movieMap = new Map();
 
 //create cards
-function createMovieCard(a) {
+function createMovieCard(data) {
+  //fetch ID of HTML
+  let movieCardSection = document.querySelector('.movieCardsFlex');
+
   //create div elements in HTML and assign class
   let movieCard = document.createElement('div');
   movieCard.classList.add('movie-card');
 
   //put the div elements that movieCard creates into HTML
   movieCard.innerHTML = `
-    <div class="card">
-      <img src="https://image.tmdb.org/t/p/w200/${a.poster_path}" class="card-img-top" alt="..." />
-      <p id="cardtitle">${a.title}</p>
-      <p class="votes">⭐️ <span class="rating-color">${a.vote_average}</span></p>
-      <p class="overview">${a.overview}</p>
-    </div>`;
+    <img src="https://image.tmdb.org/t/p/w200/${data.poster_path}" class="card-img" />
+    <p id="cardtitle">${data.title}</p>
+    <p class="rating">⭐️ <span class="rating-color">${data.vote_average}</span></p>
+    <p class="overview">${data.overview}</p>
+    `;
 
-  //make the created movieCards subordinate to movieCardPost
-  movieCardPost.appendChild(movieCard);
+  //make the created movieCards subordinate to moiveCardSection
+  movieCardSection.appendChild(movieCard);
 
   //assign titles & cards to Map elements
-  movieMap.set(a.title, movieCard);
+  movieMap.set(data.title, movieCard);
 
   //use addEventListener for onclick action
   movieCard.addEventListener('click', () => {
-    const idNumber = a.id;
-    alert('Movie ID Number is ' + a.id + '.');
+    const idNumber = data.id;
+    alert('Movie ID Number is ' + data.id + '.');
   });
 }
 
-//fetch search-input and search-btn ids to enalbe the search function
-const searchButton = document.querySelector('#search-btn');
+//fetch search-input and search-btn ids to enable the search function
+const searchButton = document.getElementById('search-btn');
 const searchInput = document.getElementById('search-input');
-console.log(searchButton);
 
 function search() {
   let text = searchInput.value;
-  topMovies.forEach((a) => {
-    // let movieTitle = a['title'];
-
+  movies.forEach((data) => {
     //make the input characters all lowercase and match with cards
-    if (a.title.toLowerCase().includes(text.toLowerCase())) {
-      //show the cards that match with the title
-      movieMap.get(a.title).style.display = 'block';
+    if (
+      data.title.toLowerCase().includes(text.toLowerCase()) ||
+      data.overview.toLowerCase().includes(text.toLowerCase())
+    ) {
+      //show the cards that match with the title or keywords
+      movieMap.get(data.title).style.display = 'block';
     } else {
-      //hide the cards that doesn't match with the title
-      movieMap.get(a.title).style.display = 'none';
+      //hide the cards that don't match with the title
+      movieMap.get(data.title).style.display = 'none';
     }
   });
 }
 
 //run 'search' function when clicking the search button
-searchButton.addEventListener('click', (e) => {
-  e.preventDefault();
+searchButton.addEventListener('click', (searchEvent) => {
+  searchEvent.preventDefault();
   search();
-});
-
-//run 'search' function when entering the text input
-searchInput.addEventListener('keyup', function (event) {
-  if (event.key === 'Enter') {
-    search();
-  }
 });
 
 //search input status when refreshed
